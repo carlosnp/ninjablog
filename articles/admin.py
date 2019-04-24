c3ff44a115b4bd7a698dc5800e4c159b357affcb
machinery <machinery.e@gmail.com>
2019-04-24 10:09:03 -0400

    ajustes en el modelo, en los estilos y en el gitignore

--- articles/admin.py
+++ articles/admin.py
@@ -4,4 +4,8 @@ from django.contrib import admin
 # Project
 from .models import Article
 
-admin.site.register(Article)
+class ArticleModelAdmin(admin.ModelAdmin):
+	list_display = ('title', 'data', 'updated')
+	search_fields = ["title", "body"]
+
+admin.site.register(Article, ArticleModelAdmin)
--- articles/models.py
+++ articles/models.py
@@ -5,9 +5,13 @@ class Article(models.Model):
 	title 	= models.CharField("Titulo",max_length=100)
 	slug 	= models.SlugField()
 	body	= models.TextField("Contenido")
-	data 	= models.DateTimeField("Fecha de creación", auto_now_add=True)
+	data 	= models.DateTimeField("Fecha de creación", auto_now=False, auto_now_add=True)
+	updated = models.DateTimeField("Fecha de Actualización",auto_now=True, auto_now_add=False)
 	# thumbnail
 	# Author
-
+	class Meta:
+		verbose_name = 'Artículo'
+		verbose_name_plural = 'Artículos'
+	
 	def __str__(self):
 		return str(self.title)
