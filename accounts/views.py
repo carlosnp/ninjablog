1f5d95a38590ff317b8ddc22e4c6c22e16dc672f
machinery <machinery.e@gmail.com>
2019-04-24 14:57:16 -0400

    registro de usuario funciona

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,20 @@
 # Django
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Registro de usuario
 def signupView(request):
 	template_name = 'signup.html'
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		# Si el formulario es valido
+		if form.is_valid():
+			form.save()
+			return redirect("home")
+	else:
+		form = UserCreationForm()
+	
 	context = {
+		"form": form,
 	}
 	return render(request, template_name, context)
